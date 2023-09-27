2000d806c04d075cf3d8d69d917d2261a815d8a4
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-09-27 12:54:22 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let answer = -1;
+    for (i=0; i<num_list.length; i++) {
+        if(num_list[i] < 0) {
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
