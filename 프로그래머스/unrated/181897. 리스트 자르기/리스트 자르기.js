accbb2b98eb6d01177dd74a1fc6714c88fd97dc9
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-10-07 13:01:34 +0900

    [unrated] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,16 @@
+function solution(n, [a,b,c], num_list) {
+    switch (n) {
+        case 1:
+            return num_list.slice(0,b+1);
+            break;
+        case 2:
+            return num_list.slice(a);
+            break;
+        case 3:
+            return num_list.slice(a,b+1);
+            break;
+        case 4:
+            return num_list.slice(a,b+1).filter((_,i) => i%c === 0);
+            break;
+    }
+}
