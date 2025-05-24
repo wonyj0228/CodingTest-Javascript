2ce6e7886eea418671f05dd3d918b4f9120b8ccf
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-05-24 18:47:50 +0900

    [Bronze V] Title: 개, Time: 88 ms, Memory: 9284 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,9 @@
+function solution() {
+  console.log(`|\\_/|`);
+  console.log(`|q p|   /}`);
+  console.log(`( 0 )"""\\`);
+  console.log(`|"^"\`    |`);
+  console.log(`||_/=\\\\__|`);
+}
+
+solution();
