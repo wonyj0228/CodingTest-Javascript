c788368ee223a26a2739dea425985a93ff6504d9
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-05-26 14:04:27 +0900

    [Bronze V] Title: 문자와 문자열, Time: 108 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,20 @@
+// 문제 풀이
+function solution(input) {
+  console.log(input[0][parseInt(input[1]) - 1]);
+}
+
+/* readline Module */
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
