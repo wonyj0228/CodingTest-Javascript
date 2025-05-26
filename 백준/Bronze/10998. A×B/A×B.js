99d04827ae0a14c7a18760ca91130c9d47c5ba35
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-05-26 13:59:02 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,24 @@
+// 문제 풀이
+function solution(input) {
+  const [a, b] = input;
+  console.log(a * b);
+}
+
+/* readline Module */
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+let list = [];
+
+rl.on('line', function (line) {
+  input = line;
+  rl.close();
+}).on('close', function () {
+  list = input.split(' ').map((v) => parseInt(v));
+  solution(list);
+  process.exit();
+});
