8f40ad85b65ddadbb8c690c6f9b58b9b7ffe8cd5
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-05-30 22:35:56 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 9712 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -3,8 +3,10 @@ function solution(input) {
   const inputArr = [...input[1]].map((str) => str.charCodeAt(0) - 96);
 
   let sum = 0;
+  let pow = 1;
   for (let i = 0; i < inputArr.length; i++) {
-    sum += inputArr[i] * 31 ** i;
+    sum += (inputArr[i] * pow) % 1234567891;
+    pow = (pow * 31) % 1234567891;
   }
   console.log(sum % 1234567891);
 }
